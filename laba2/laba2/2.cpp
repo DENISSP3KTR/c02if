c18131a89c8b5c6a5985db5d15d739be2a56c9ab
DENISSP3KTR <54879762+DENISSP3KTR@users.noreply.github.com>
2020-02-12 16:15:48 +0900

    Update 2.cpp

--- laba2/laba2/2.cpp
+++ laba2/laba2/2.cpp
@@ -9,7 +9,7 @@
 //	if ((x>='0') && (x<='9')) cout << "DIGIT" << endl;
 //	else if ((x >= 'A') && (x <= 'Z')) cout << "CAPITAL" << endl;
 //	else if ((x >= 'a') && (x <= 'z')) cout << "LOWERCASE" << endl;
-//	else cout<<"ERROR"<<endl;
+//	else cout<<"NON‐ALPHANUMERIC"<<endl;
 //	system("pause");
 //	return 0;
 //}
